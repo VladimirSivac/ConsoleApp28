2f6684c0051b89c53faef282606c6c93a828a3fe
VladimirSivac <morndes@gmail.com>
2025-03-10 19:58:20 +0200

    project created

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    /// <summary>
+    /// Компьютерный практикум № 28 (05.03.2025)
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
