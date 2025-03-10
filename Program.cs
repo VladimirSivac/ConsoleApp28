76498f24311dc85f2e0b7a5bcba3240febe668ae
VladimirSivac <morndes@gmail.com>
2025-03-10 20:20:08 +0200

    task-3 done

--- Program.cs
+++ Program.cs
@@ -11,9 +11,51 @@ namespace Project
     /// </summary>
     internal class Program
     {
+        // task3
         static void Main(string[] args)
         {
-            
+            int[] array = { 1, -5, 6, -19, 25, -1, 12, 21 };
+
+            Console.WriteLine("Вывод массива до перестановки: ");
+            ArrayPrinting(array);
+            Console.WriteLine();
+
+            int maxIndex = FindMaxElementIndex(array);
+            SwapMaxFirstElements(array, maxIndex);
+
+            Console.WriteLine("Вывод массива после перестановки максимального и 1-го элементов: ");
+            ArrayPrinting(array);
+
+            Console.ReadKey();
+        }
+
+        static int FindMaxElementIndex(int[] array)
+        {
+            int maxIndex = 0;
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] > array[maxIndex])
+                {
+                    maxIndex = i;
+                }
+            }
+            return maxIndex;
+        }
+
+        static void ArrayPrinting(int[] array)
+        {
+            foreach (int item in array)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+
+        static void SwapMaxFirstElements(int[] array, int maxIndex)
+        {
+            int temp = array[0];
+            array[0] = array[maxIndex];
+            array[maxIndex] = temp;
         }
     }
 }
